e43344fe6e2796ceb7712de13622d897a0d45c6d
Ali Mohamed Ali <am6064029@gmail.com>
2020-06-23 00:21:14 +0200

    roles& permissions by laratrust

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('users')->truncate();
+
+        $user = User::create([
+        	'first_name' => 'super',
+        	'last_name' => 'admin',
+        	'email' => 'super_admin@app.com',
+        	'password' => bcrypt('123456'),
+
+        ]);
+        $user->attachRole('super_admin');
+
+
+    } //end of run
+}
